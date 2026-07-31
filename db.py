f1d4d6ec769d050ab46f8218ca7a0db648a496ce
matijasdev <nizmas66@gmail.com>
2026-07-31 06:51:14 +0200

    Move database URL to environment variables

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine, MetaData
+from config import DATABASE
 
 metadata = MetaData()
 
-db = "postgresql+psycopg2://postgres:1234@localhost:5432/postgres"
+db = DATABASE
 
 engine = create_engine(db)
 
