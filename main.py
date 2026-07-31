9085e3f732307c013f209bfc957d07cd01bbd640
matijasdev <nizmas66@gmail.com>
2026-07-31 08:04:47 +0200

    Add IP-based rate limiting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from queries import get_original_link, increment_click_count, get_stats
 from utils import process_url, extract_code
 from config import DOMAIN
@@ -7,9 +9,15 @@ import models          # register tables
 
 app = Flask(__name__)
 
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app
+    )
+
 metadata.create_all(engine)
 
 @app.route("/", methods=["GET", "POST"])
+@limiter.limit("30/minute")
 def home():
     if request.method == "GET":
         return render_template("index.html")
@@ -25,6 +33,7 @@ def home():
 
 
 @app.route("/stats")
+@limiter.limit("50/minute")
 def check_stats():
     user_input = request.args.get("query", "").strip()
 
